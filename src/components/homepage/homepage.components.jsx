e53e2ca552f74c89a37798f8794e04173984ca04
Ogbinaka Emmanuel <ogbinakaemmanuel@gmail.com>
2020-01-28 23:42:44 +0100

    styled the menu-item component and some component architecture changes, time to rest a little

--- src/components/homepage/homepage.components.jsx
+++ src/components/homepage/homepage.components.jsx
@@ -1,67 +1,13 @@
 import React from "react";
 import './homepage.styles.scss';
+import Directory from '../directory/directory.component';
 
-const HomePage = () => {
-    return(
-        <>
+
+const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Hats</h1>
-          <span className="subtitle">shop now</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
-
-  <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Jackets</h1>
-          <span className="subtitle">shop now</span>
-        </div>
-      </div>
-    </div>
-  </div>;
-
-  <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Shoes</h1>
-          <span className="subtitle">shop now</span>
-        </div>
-      </div>
-    </div>
-  </div>;
-
-  <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Womens</h1>
-          <span className="subtitle">shop now</span>
-        </div>
-      </div>
-    </div>
-  </div>;
-
-  <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Mens</h1>
-          <span className="subtitle">shop now</span>
-        </div>
-      </div>
-    </div>
-  </div>
-</>
-    )
-
-};
+)
 
 
 export default HomePage;
