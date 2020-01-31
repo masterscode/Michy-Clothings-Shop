aa3c27b9cb0833783c661b98cd184dccc92ac960
Ogbinaka Emmanuel <ogbinakaemmanuel@gmail.com>
2020-01-31 10:30:02 +0100

    Added some more components and their styles, firebase integration next

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./sign-in.style.scss";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from '../custom-button/custom-button.component';
+
+
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an accout</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label  = 'email'
+            required
+          />
+          
+          <FormInput
+            type="password"
+            handleChange={this.handleChange}
+            name="password"
+            label = 'password'
+            value={this.state.password}
+          />
+          
+          <CustomButton type = 'submit'>Submit Form</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
